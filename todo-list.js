cf814dca345c4f61aa816578ec1efee688157138
lpdft <772346730@qq.com>
2019-12-21 22:17:55 +0800

    [ruyue] feat-todo list: finish use button and enter to add task

---
+++ todo-list.js
@@ -0,0 +1,11 @@
+function addTask(event) {
+  var taskList = document.getElementsByClassName('task-list')[0];
+  var taskInfo = document.getElementsByClassName('task')[0].value;
+  var row = 
+    "<input type='checkbox' class='checked'>" +
+    "<span>" + taskInfo + "</span>";
+  var li = document.createElement("li");
+  li.innerHTML = row;
+  taskList.appendChild(li);
+} 
+
