19bf0dad182ceeded5712300cc170da059d18fc8
lpdft <772346730@qq.com>
2019-12-21 22:26:00 +0800

    [ruyue] fixed-todo list: fix will not add task when input is enpty

--- todo-list.js
+++ todo-list.js
@@ -1,11 +1,14 @@
 function addTask(event) {
   var taskList = document.getElementsByClassName('task-list')[0];
   var taskInfo = document.getElementsByClassName('task')[0].value;
-  var row = 
+  if(taskInfo) {
+    var row = 
     "<input type='checkbox' class='checked'>" +
     "<span>" + taskInfo + "</span>";
   var li = document.createElement("li");
   li.innerHTML = row;
   taskList.appendChild(li);
+  }
 } 
+
 
